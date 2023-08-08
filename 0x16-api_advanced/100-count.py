0e8269e016ce89a6667af813a85743d8e738840f
AyanSwarga <ayannicholas@gmail.com>
2023-08-08 17:42:55 +0100

    update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,53 +1,58 @@
 #!/usr/bin/python3
-""" A recursive function that queries the Reddit API"""
+""" Query Reddit API """
 
-import json
 import requests
 
-def count_words(subreddit, word_list, after="", count=[]):
-    """count all words"""
 
-    if after == "":
-        count = [0] * len(word_list)
+def count_words(subreddit, word_list, count_list=[], next_page=None):
+    """Request subreddit recursively using pagination
+    """
+    # convert word_list to dict with count
+    if not count_list:
+        for word in word_list:
+            count_list.append(dict({'keyword': word,
+                                    'count': 0}))
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    request = requests.get(url,
-                           params={'after': after},
-                           allow_redirects=False,
-                           headers={'user-agent': 'bhalut'})
+    # NETWORKING
+    # set custom user-agent
+    user_agent = '0x16-api_advanced-jmajetich'
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    # if page specified, pass as parameter
+    if next_page:
+        url += '?after={}'.format(next_page)
 
-    if request.status_code == 200:
-        data = request.json()
+    headers = {'User-Agent': user_agent}
 
-        for topic in (data['data']['children']):
-            for word in topic['data']['title'].split():
-                for i in range(len(word_list)):
-                    if word_list[i].lower() == word.lower():
-                        count[i] += 1
+    r = requests.get(url, headers=headers, allow_redirects=False)
 
-        after = data['data']['after']
-        if after is None:
-            save = []
-            for i in range(len(word_list)):
-                for j in range(i + 1, len(word_list)):
-                    if word_list[i].lower() == word_list[j].lower():
-                        save.append(j)
-                        count[i] += count[j]
+    if r.status_code != 200:
+        return
 
-            for i in range(len(word_list)):
-                for j in range(i, len(word_list)):
-                    if (count[j] > count[i] or
-                            (word_list[i] > word_list[j] and
-                             count[j] == count[i])):
-                        aux = count[i]
-                        count[i] = count[j]
-                        count[j] = aux
-                        aux = word_list[i]
-                        word_list[i] = word_list[j]
-                        word_list[j] = aux
+    # DATA PARSING
+    # load response unit from json
+    data = r.json()['data']
 
-            for i in range(len(word_list)):
-                if (count[i] > 0) and i not in save:
-                    print("{}: {}".format(word_list[i].lower(), count[i]))
-        else:
-            count_words(subreddit, word_list, after, count)
+    # extract list of pages
+    posts = data['children']
+    for post in posts:
+        title = post['data']['title']
+        for item in count_list:
+            title_lower = title.lower()
+            title_list = title_lower.split()
+            item['count'] += title_list.count(item['keyword'].lower())
+
+    next_page = data['after']
+    if next_page is not None:
+        return count_words(subreddit, word_list, count_list, next_page)
+    else:
+        # sort list by count
+        sorted_list = sorted(count_list,
+                             key=lambda word: (word['count'], word['keyword']),
+                             reverse=True)
+        keywords_matched = 0
+        # print keywords and counts
+        for word in sorted_list:
+            if word['count'] > 0:
+                print('{}: {}'.format(word['keyword'], word['count']))
+                keywords_matched += 1
+        return
