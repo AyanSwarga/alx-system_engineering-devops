3f0231d4db2c4a42569940227372bc2050163610
AyanSwarga <ayannicholas@gmail.com>
2023-08-08 17:20:45 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
+"""This script Query Reddit API
+to determine subreddit sub count
 """
-this scrpt is a function that queries the Reddit API and 
-returns the number of subscribers for a given subreddit
-"""
-from requests import get
+import requests
 
 def number_of_subscribers(subreddit):
+    """Request number of subscribers of subreddit
+    from Reddit API
     """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
+    # set custom user-agent
+    user_agent = '0x16-api_advanced-adeyemodanointed'
+    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
 
-    if subreddit is None or not isinstance(subreddit, str):
+    # custom user-agent avoids request limit
+    headers = {'User-Agent': user_agent}
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
-        return 0
+    # load response unit from json
+    data = r.json()['data']
+    # extract list of pages
+    pages = data['children']
+    # extract data from first page
+    page_data = pages[0]['data']
+    # return number of subreddit subs
+    return page_data['subreddit_subscribers']
